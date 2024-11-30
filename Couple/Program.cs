9cad0530f2ebc80c1c63edef03996a6c2f30c019
blh9 <blh9zen@gmail.com>
2024-12-01 01:57:06 +0300

    Добавьте файлы проекта.

---
+++ Couple/Program.cs
@@ -0,0 +1,38 @@
+﻿public class MainClass
+{
+    public static void Main()
+    {
+
+        myFunction([5, 1, 3, 1, 3, 5, 52]);
+
+    }
+
+
+    static void myFunction(int[] arr)
+    {
+        Array.Sort(arr);
+
+
+        for (int i = 0; i < arr.Length; i += 2)
+        {
+
+
+            if (i + 1 < arr.Length)
+            {
+                if (arr[i] != arr[i + 1])
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine(arr[arr.Length - 1]);
+            }
+
+        }
+
+
+
+    }
+
+}
